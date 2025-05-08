78a5df9bd6e65609931b87de8c5d6d95de8bfdcb
Joseph <j.mburu.pro@gmail.com>
2025-05-08 22:25:20 +0300

    init

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed=None):
+        '''takes a name as an argument and saves it to self.name'''
+        self.name = name
+        '''takes a breed as an argument and saves it to self.breed'''
+        self.breed = breed
+        '''sets self.breed = "Mutt" when no breed specified'''
+        if not breed:
+            self.breed = "Mutt"
